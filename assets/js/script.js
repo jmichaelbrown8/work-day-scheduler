969c64ddcbd53f17e7256e11b5c305a6a9159dc3
J. Michael Brown <jmichaelbrown8@gmail.com>
2021-12-02 22:31:03 -0800

    Render loads the page from localStorage, save button click stores the line to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-const dayFormat = "YYYY-MM-DD";
+const dateFormat = "YYYY-MM-DD";
+const mainEl = $('main');
 
 var today = moment();
 var now = today;
@@ -32,17 +33,15 @@ function initialize() {
     // make calendar icon clickable for a jqueryui calendar dialog switcher
 
     // check localstorage for data to display
-    // localStorage = {
-    //     "2021-11-30": {
-    //         "8": "Meeting with the boss",
-    //         "14": "Coffee break"
-    //     },
-    //     "2021-12-01": {}
-    // }
+    // stored in objects with a date in YYYY-MM-DD format for the key
+    var data = getLocalStorage();
     
     // write out data to page
-    renderPage(now);
+    renderPage(today, data);
 
+    mainEl.on('click', 'button', handleClick);
+
+    mainEl.on('change', "input", handleChange);
 }
 
 /**
@@ -51,9 +50,11 @@ function initialize() {
  * @param {object} data - the data to initialize on the page
  */
 function renderPage(mmt, data) {
-    // render a disabled page and a message if it's a non-working day
+    // TODO: render a disabled page and a message if it's a non-working day
 
-    var main = $('main');
+    if (!data) {
+        data = {};
+    }
 
     // render a normal working day if not
     // loop through the hours
@@ -61,7 +62,7 @@ function renderPage(mmt, data) {
         let style = "";
         let time = moment(workingHours[i], "H").format("h:mm A");
         let disabled = "";
-        let text = "";
+        let text = data[workingHours[i]] || "";
         
         // if it's earlier than now (by day, or hour on same day), render red
         if (workingHours[i] < mmt.hour()) {
@@ -83,17 +84,15 @@ function renderPage(mmt, data) {
         if (awayHours.includes(workingHours[i])) {
             style = "dark";
             disabled = "disabled";
-            text = "away";
+            text = "Away";
         }
 
-        main.append(`
+        mainEl.append(`
         <div class="alert-${style} d-flex flex-row justify-content-between align-items-center py-1">
             <span class="px-1 time-label">${time}</span>
             <div class="input-group">
-                <input type="text" class="flex-grow-1" placeholder="+" ${disabled} value="${text}" />
-                <button type="button" class="btn" aria-label="Save" ${disabled} >
-                    <i class="fa fa-save"></i>
-                </button>
+                <input id="${workingHours[i]}" type="text" class="flex-grow-1" placeholder="+" ${disabled} value="${text}" />
+                <button type="button" class="btn fa fa-save" aria-label="Save" ${disabled} ></button>
             </div>
         </div>
         `);
@@ -103,12 +102,41 @@ function renderPage(mmt, data) {
 
 // button click function to save line into the localstorage object
 function handleClick(event) {
+    // save the closest input to the localStorage in the given key
+    let date = today.format(dateFormat);
+    let inputEl = $(event.target).siblings("input");
 
+    let id = inputEl.attr('id');
+    let data = inputEl.val();
+    let currentStorage = getLocalStorage(date) || {};
+    currentStorage[id] = data;
+    storeLocalStorage(date, currentStorage);
+}
+
+// text change function to trigger validation that the page has changed
+function handleChange(event) {
+    console.log(event);
 }
 
 // function to get data out of localstorage for the given date
+function getLocalStorage(date) {
+    if (!date) {
+        date = now.format(dateFormat);
+    }
 
-// function to save data into localstorage for the given date
+    return JSON.parse(localStorage.getItem(date));
+}
+
+// function to save fully formed data into localstorage for the given date
+function storeLocalStorage(date, dataObject) {
+    if (!date || !dataObject) {
+        return "fail";
+    }
+
+    localStorage.setItem(date, JSON.stringify(dataObject));
+
+    return "success";
+}
 
 // function to change the date on the page
 
